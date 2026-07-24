0ef019d3ff646691845d2ec929a4f707ffacb98e
AidillahZaneta-bit <zanettaaridillah@gmail.com>
2026-07-24 07:50:53 +0700

    Add files via upload

---
+++ zaneta.js
@@ -0,0 +1,4 @@
+console.log("Aidillah Wahyu Zaneta")
+console.log("08 September 2011")
+console.log("SMP Negeri 1 Kwadungan")
+console.log("Saya memilih jurusan RPL di SMK Telkom Malang, karena saya melihat besar potensi dari lulusan jurusan ini dapat bekerja di beberapa bidang yang memang saya minati. Salah satunya adalah saya ingin menjadi Data Analyst")
