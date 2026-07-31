3f782b6db1d494cba8d0ea2acdfc65aa409bbea4
AidillahZaneta-bit <zanettaaridillah@gmail.com>
2026-07-31 08:01:34 +0700

    Add files via upload

---
+++ karyawan.js
@@ -0,0 +1,15 @@
+//Deklarasi Objek 
+const Karyawan = {
+    Nama: "Cia Cici",
+    Umur: 23,
+    Gaji: 800000,
+    statusKarTap: false,
+    Jabatan: null
+}
+
+//Output
+console.log (`Nama: ${Karyawan.Nama}`);
+console.log (`Umur: ${Karyawan.Umur}`);
+console.log (`Gaji: ${Karyawan.Gaji}`);
+console.log (`StatusKarTap: ${Karyawan.statusKarTap}`);
+console.log (`Jabatan: ${Karyawan.Jabatan}`);
